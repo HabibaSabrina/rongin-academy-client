ca7fca0d1b7d42b6eecc585755b99c4535c6eeff
HabibaSabrina <habibasabrina92@gmail.com>
2023-06-09 16:11:16 +0600

    tanstack query has been implemented

--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,22 @@ import {
 import router from './pages/routes/Routes.jsx';
 import { HelmetProvider } from 'react-helmet-async';
 import AuthProvider from './providers/AuthProvider';
+
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
       <HelmetProvider>
-        <div>
-          <RouterProvider router={router}></RouterProvider>
-        </div>
+      <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router}></RouterProvider>
+      </QueryClientProvider>
       </HelmetProvider>
     </AuthProvider>
   </React.StrictMode>,
--- src/pages/layout/Dashboard.jsx
+++ src/pages/layout/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import DashboardHeader from '../Dashboard/DashboardHeader/DashboardHeader';
+import { Outlet } from 'react-router-dom';
 
 const Dashboard = () => {
     return (
         <div>
             <DashboardHeader></DashboardHeader>
+            <Outlet></Outlet>
         </div>
     );
 };
