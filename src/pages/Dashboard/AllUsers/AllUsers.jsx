d30d642213ce58ef0c61c7759bf2dce3300f78fc
HabibaSabrina <habibasabrina92@gmail.com>
2023-06-10 01:37:20 +0600

    admin and instructor role has been established

--- src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,12 +1,51 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
-import UserRow from './UserRow';
 import '../../components/styles/table.css'
+import Swal from 'sweetalert2';
 const AllUsers = () => {
-    const {data: users = [], refetch} = useQuery(['users'], async() => {
+    const { data: users = [], refetch } = useQuery(['users'], async () => {
         const res = await fetch('http://localhost:5000/users')
         return res.json();
     })
+    const handleSelectAdmin = user =>{
+        fetch(`http://localhost:5000/users/admin/${user._id}`, {
+            method: 'PATCH'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            if(data.modifiedCount){
+                refetch();
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: `${user.name} is an Admin Now!`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
+            }
+        })
+    }
+    const handleSelectInstructor = user =>{
+        fetch(`http://localhost:5000/users/instructor/${user._id}`, {
+            method: 'PATCH'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            if(data.modifiedCount){
+                refetch();
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: `${user.name} is an Instructor Now!`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
+            }
+        })
+    }
+    
     return (
         <div className='table-bg p-20 bg-[#f9f9e3]'>
             <div className=" mx-32">
@@ -21,7 +60,27 @@ const AllUsers = () => {
                     </thead>
                     <tbody>
                         {
-                            users.map(user => <UserRow key={user._id} user={user}></UserRow>)
+                            users.map(user => <tr key={user._id} className='font-semibold text-center border-b-2 border-[#C5CBE3]  bg-[#f9f9e3]'>
+                                <td>
+                                    <div className="avatar">
+                                        <div className="rounded w-24 h-24">
+                                            {user.photo && <img src={user.photo} className='p-2 rounded-xl' alt={user.name} />}
+                                        </div>
+                                    </div>
+                                </td>
+                                <td className='border-x-2 border-[#C5CBE3]'>
+                                    {user.name && user.name}
+                                </td>
+                                <td className='border-x-2 border-[#C5CBE3]'>{user.email}</td>
+                                <td>
+                                    {
+                                        user.role === 'admin' ? <button disabled className='p-3 px-6 mr-4 bg-[#F13C20] bg-opacity-30 font-semibold text-white rounded-full'>Admin</button> : <button onClick={() => handleSelectAdmin(user)} className='p-3 px-6 mr-4 bg-[#F13C20] hover:bg-blue-800  font-semibold text-white rounded-full'>Make Admin</button>
+                                    }
+                                    {
+                                        user.role === 'instructor' ? <button disabled className='p-3 px-6 mr-4 bg-[#F13C20] bg-opacity-30 font-semibold text-white rounded-full'>Instructor</button> : <button onClick={() => handleSelectInstructor(user)} className='p-3 px-6 bg-[#F13C20] hover:bg-blue-800  font-semibold text-white rounded-full'>Make Instructor</button>
+                                    }
+                                </td>
+                            </tr>)
                         }
                     </tbody>
 
--- src/pages/Dashboard/AllUsers/UserRow.jsx
+++ src/pages/Dashboard/AllUsers/UserRow.jsx
@@ -3,6 +3,25 @@ import { Link } from 'react-router-dom';
 
 const UserRow = ({user}) => {
     const {name, email, photo } = user
+    const handleSelectAdmin = user =>{
+        fetch(`http://localhost:5000/users/admin/${user._id}`, {
+            method: 'PATCH'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            if(data.modifiedCount){
+                refetch();
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: `${user.name} is an Admin Now!`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
+            }
+        })
+    }
     return (
         <tr className='font-semibold text-center border-b-2 border-[#C5CBE3]  bg-[#f9f9e3]'>
         <td>
@@ -16,8 +35,14 @@ const UserRow = ({user}) => {
             {name && name}
         </td>
         <td className='border-x-2 border-[#C5CBE3]'>{email}</td>
-        <td><Link to=''><button className='p-3 px-6 mr-4 bg-[#F13C20] hover:bg-blue-800  font-semibold text-white rounded-full'>Admin</button></Link>
-        <Link to=''><button className='p-3 px-6 bg-[#F13C20] hover:bg-blue-800  font-semibold text-white rounded-full'>Instructor</button></Link></td>
+        <td>
+            {
+                user.role === 'admin' ? 'admin': <button onClick={() => handleSelectAdmin(user)} className='p-3 px-6 mr-4 bg-[#F13C20] hover:bg-blue-800  font-semibold text-white rounded-full'>Admin</button>
+            }
+        {
+            user.role === 'instructor' ? 'instructor' : <button className='p-3 px-6 bg-[#F13C20] hover:bg-blue-800  font-semibold text-white rounded-full'>Instructor</button>
+        }
+        </td>
     </tr>
     );
 };
